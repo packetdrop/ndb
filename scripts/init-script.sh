ef13a5f8ffba960f128123299ce035baa3c5ef87
Anil Vishnoi <vishnoianil@gmail.com>
2020-03-23 23:30:35 -0400

    Print timestamp in entry point script
    Signed-off-by: Anil Vishnoi <vishnoianil@gmail.com>

--- scripts/init-script.sh
+++ scripts/init-script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Welcome to Network Debugging Container. This container contains following tools
-to help debugging the issue you are currently facing."
+to help debugging the issue you are currently facing." `date`
