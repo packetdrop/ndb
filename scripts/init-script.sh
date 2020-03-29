e57448bd7142ad218598028e3297d347c8fd8024
Anil Vishnoi <vishnoianil@gmail.com>
2020-03-29 16:23:35 -0700

    Allow container to live for the specified time period, if needed
    Signed-off-by: Anil Vishnoi <vishnoianil@gmail.com>

--- scripts/init-script.sh
+++ scripts/init-script.sh
@@ -1,4 +1,28 @@
 #!/bin/sh
 
-echo "Welcome to Network Debugging Container. This container contains following tools
-to help debugging the issue you are currently facing." `date`
+echo "Welcome to Network Debugging Container. This container contains tools
+to help debugging the networking related issues." `date`
+
+echo " List of tools currently installed:
+# net-tools (ifconfig )
+# traceroute
+# ethtool
+# mtr
+# iperf
+#  nmap
+# strace
+# socat
+# iproute
+# bind-utils (nslookup)
+# wireshark
+# nmon
+# bmon
+# openssl
+"
+
+if [ "$#" -lt 1 ]; then
+  echo " Container is going dead, but don't worry your tap (nic) is still hooked."
+else
+  echo "Sleeping for $1, enjoy debugging your issue and use the container if you want to leverage the tools this container contains."
+  sleep "$1"
+fi
